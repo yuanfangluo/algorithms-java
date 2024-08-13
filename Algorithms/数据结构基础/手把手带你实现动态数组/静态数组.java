1f580edf84fc0f4636b39d13205d4b873b691653
yuanfangluo <shuishengluo@gmail.com>
2024-08-13 22:00:07 +0800

    add

---
+++ Algorithms/数据结构基础/手把手带你实现动态数组/静态数组.java
@@ -0,0 +1,16 @@
+package Algorithms.数据结构基础.手把手带你实现动态数组;
+
+public class 静态数组 {
+    // 静态数组是指在创建数组的时候就确定了这个数组用来存放什么类型的元素以及数组的大小
+    public static void main(String[] args) {
+        // 静态数组的创建
+        int[] arr = new int[10];
+        // 静态数组的访问
+        arr[0] = 1;
+        arr[1] = 2;
+        arr[2] = 3;
+        arr[3] = 4;
+        arr[4] = 5;
+        arr[5] = 6;
+    }
+}
